4334acf63baada00fdea2750a56c402fd00a5f51
Necmi Gunduz <necmigunduz>
2021-01-07 16:54:27 +0300

    Add more solution

--- enumerables.rb
+++ enumerables.rb
@@ -14,6 +14,27 @@ module Enumerable
     def Enumerable.my_all?
         yield("a",2,3)
     end
+
+    def Enumerable.my_any?
+        yield(2,4)
+    end
+
+    def Enumerable.my_none? 
+        yield
+    end
+
+    def Enumerable.my_count
+        yield(Array)
+    end
+
+    def Enumerable.my_map
+        yield
+    end
+    
+    def Enumerable.my_inject
+        sum = 0
+        yield([1,2,3,45])
+    end
 end
 
 Enumerable.my_each {|str, num| puts str + ' ' + num.to_s} 
@@ -23,3 +44,44 @@ Enumerable.my_each_with_index { |fruit| puts fruit[2] }
 Enumerable.my_select {|friend| puts friend - ["Leo"]}
 
 Enumerable.my_all? { |element| puts element.is_a?(Integer) }
+
+Enumerable.my_any? do |num|
+    if num%2 != 0 
+        puts "All numbers are odd"
+    elsif num%2 != 0 && num%2 == 0
+        puts "There is at least one odd and one even number"
+    else
+        puts "All numbers are even"
+    end
+end
+
+a = ["an","bearw","aaaaaa"]
+
+Enumerable.my_none?  do
+    for word in a
+        if word.length >= 5
+            puts true
+        else
+            puts false
+        end
+    end
+end
+
+b = [1,2,3,4,5,"rr"]
+Enumerable.my_count do |i|
+    counts = 0
+    (0...b.length).each do
+        if i 
+            counts += 1
+        end
+    end
+    puts counts
+end
+
+z = ["a","b","c"]
+
+Enumerable.my_map { z.each {|letter| puts letter.upcase } }
+
+d = [1,2,3,4,5]
+s = 0
+Enumerable.my_inject {d.each {|i|  puts s += i} }
