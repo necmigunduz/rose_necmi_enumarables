c37c60ef09cf6e32b3b51fc8dc1639b8f7134cfe
Necmi Gunduz <necmigunduz>
2021-01-07 12:15:52 +0300

    Add Exercise 5

--- enumerables.rb
+++ enumerables.rb
@@ -8,7 +8,11 @@ module Enumerable
     end
 
     def Enumerable.my_select
-        yield
+        yield(['Sharon', 'Leo', 'Leila', 'Brian', 'Arun'])
+    end
+
+    def Enumerable.my_all?
+        yield("a",2,3)
     end
 end
 
@@ -16,7 +20,6 @@ Enumerable.my_each {|str, num| puts str + ' ' + num.to_s}
 
 Enumerable.my_each_with_index { |fruit| puts fruit[2] }
 
-friends = ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun']
-Enumerable.my_select  do 
-    friends.select {|friend| puts friend unless friend == 'Leo'}
-end
+Enumerable.my_select {|friend| puts friend - ["Leo"]}
+
+Enumerable.my_all? { |element| puts element.is_a?(Integer) }
