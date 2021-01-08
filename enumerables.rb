f14cde82414ef546e48da26cc4adbf5cb91b5ad7
Necmi Gunduz <necmigunduz>
2021-01-08 11:41:20 +0300

    Add first three tasks

--- enumerables.rb
+++ enumerables.rb
@@ -1,87 +1,47 @@
-module Enumerable  
-    def Enumerable.my_each
-      yield('hello', 99)
-    end
+# Task-1 my_each exercise
 
-    def Enumerable.my_each_with_index
-        yield(['apple', 'banana', 'strawberry', 'pineapple'])
-    end
-
-    def Enumerable.my_select
-        yield(['Sharon', 'Leo', 'Leila', 'Brian', 'Arun'])
-    end
-
-    def Enumerable.my_all?
-        yield("a",2,3)
-    end
-
-    def Enumerable.my_any?
-        yield(2,4)
-    end
-
-    def Enumerable.my_none? 
-        yield
-    end
-
-    def Enumerable.my_count
-        yield(Array)
-    end
-
-    def Enumerable.my_map
-        yield
-    end
-    
-    def Enumerable.my_inject
-        sum = 0
-        yield([1,2,3,45])
-    end
-end
-
-Enumerable.my_each {|str, num| puts str + ' ' + num.to_s} 
-
-Enumerable.my_each_with_index { |fruit| puts fruit[2] }
-
-Enumerable.my_select {|friend| puts friend - ["Leo"]}
-
-Enumerable.my_all? { |element| puts element.is_a?(Integer) }
-
-Enumerable.my_any? do |num|
-    if num%2 != 0 
-        puts "All numbers are odd"
-    elsif num%2 != 0 && num%2 == 0
-        puts "There is at least one odd and one even number"
+module Enumerable
+  def my_each
+    if block_given?
+      self.length.times {|e| yield(self[e])}
     else
-        puts "All numbers are even"
+      to_enumerator(:my_each)
     end
+  end
+
+# Test for my_each
+# [1,2,3].my_each {|e| p "#{e}"}
+      
+# Task-2 my_each_with_index
+
+def my_each_with_index
+  if block_given?
+    (0...self.length).each {|index| yield(self[index],index)}
+  else
+    to_enumerator(:my_each_with_index)
+  end
 end
 
-a = ["an","bearw","aaaaaa"]
+# Test for my_each
+# [1,2,3].my_each_with_index {|e,i| p "#{e}:#{i}"}
 
-Enumerable.my_none?  do
-    for word in a
-        if word.length >= 5
-            puts true
-        else
-            puts false
-        end
+# Task-3 my_select
+def my_select
+  if block_given?
+    arr=Array.new
+    for element in self do
+      arr.push(element) if yield(element)
     end
+    arr
+  else
+    to_enum(:my_select)
+  end
 end
 
-b = [1,2,3,4,5,"rr"]
-Enumerable.my_count do |i|
-    counts = 0
-    (0...b.length).each do
-        if i 
-            counts += 1
-        end
-    end
-    puts counts
+# Test for my_select
+ p([1, 2, 3, 4, 5, 6, 7, 8].my_select { |x| x.even? })
+
+
+
+
 end
-
-z = ["a","b","c"]
-
-Enumerable.my_map { z.each {|letter| puts letter.upcase } }
-
-d = [1,2,3,4,5]
-s = 0
-Enumerable.my_inject {d.each {|i|  puts s += i} }
